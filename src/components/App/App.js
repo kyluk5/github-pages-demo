492eb34055b3e07eb7cf3cda860db494f8bb55d9
Andrii Kulyk <kyluk5@gmail.com>
2020-10-01 17:10:39 +0300

    layout almoust done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 // import Start from "../../containers/StartPage/Start";
-import QuestionsPage from "../../containers/QuestionPage/QuestionPage";
-// import TotalScore from "../../containers/TotalScorePage/TotalScore";
+// import QuestionsPage from "../../containers/QuestionPage/QuestionPage";
+import TotalScore from "../../containers/TotalScorePage/TotalScore";
 
 function App() {
   return (
     <>
       {/* <Start /> */}
-      <QuestionsPage />
-      {/* <TotalScore /> */}
+      {/* <QuestionsPage /> */}
+      <TotalScore />
     </>
   );
 }
