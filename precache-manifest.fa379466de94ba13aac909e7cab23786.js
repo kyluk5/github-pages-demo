81bcbc2d5ece45f199b4e1d525d60e99344f3cba
Andrii Kulyk <kyluk5@gmail.com>
2020-10-03 16:16:56 +0300

    Updates

---
+++ precache-manifest.fa379466de94ba13aac909e7cab23786.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508e87ee78144156adc813d8fafe5988",
+    "url": "/github-pages-demo/index.html"
+  },
+  {
+    "revision": "806c7999a830d4628b49",
+    "url": "/github-pages-demo/static/css/QuestionsPage.16a4e538.chunk.css"
+  },
+  {
+    "revision": "1bbc1cc00074bb9e946d",
+    "url": "/github-pages-demo/static/css/Start.2f9a7f1b.chunk.css"
+  },
+  {
+    "revision": "ba94c08ffbbb44dcfae3",
+    "url": "/github-pages-demo/static/css/TotalScore.0266d466.chunk.css"
+  },
+  {
+    "revision": "dc375d34c7cd9860d608",
+    "url": "/github-pages-demo/static/css/main.c5a4f291.chunk.css"
+  },
+  {
+    "revision": "63ce12fc1e150bd2f24a",
+    "url": "/github-pages-demo/static/js/5.cdbaeb63.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-pages-demo/static/js/5.cdbaeb63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806c7999a830d4628b49",
+    "url": "/github-pages-demo/static/js/QuestionsPage.71eb215e.chunk.js"
+  },
+  {
+    "revision": "1bbc1cc00074bb9e946d",
+    "url": "/github-pages-demo/static/js/Start.0292b6d2.chunk.js"
+  },
+  {
+    "revision": "ba94c08ffbbb44dcfae3",
+    "url": "/github-pages-demo/static/js/TotalScore.33c71724.chunk.js"
+  },
+  {
+    "revision": "dc375d34c7cd9860d608",
+    "url": "/github-pages-demo/static/js/main.4525704c.chunk.js"
+  },
+  {
+    "revision": "9b053328c002495d835b",
+    "url": "/github-pages-demo/static/js/runtime-main.e2a7dd40.js"
+  },
+  {
+    "revision": "f7017fbebcdaa8813f291ba7e46bf0ba",
+    "url": "/github-pages-demo/static/media/Correct.f7017fbe.svg"
+  },
+  {
+    "revision": "03b3a8470a48b4fde2e0f729fe1ff3ff",
+    "url": "/github-pages-demo/static/media/Current.03b3a847.svg"
+  },
+  {
+    "revision": "6e72b952a58282c194b4894fc4388d74",
+    "url": "/github-pages-demo/static/media/Inactive.6e72b952.svg"
+  },
+  {
+    "revision": "0a99b51406acb2d5597fcd8f54401390",
+    "url": "/github-pages-demo/static/media/Rectangle 4.0a99b514.svg"
+  },
+  {
+    "revision": "7001afac675f483b2ef470b441ef7bb8",
+    "url": "/github-pages-demo/static/media/Selected.7001afac.svg"
+  },
+  {
+    "revision": "cecfba40215c1eef8ab4a58c0e7ccec0",
+    "url": "/github-pages-demo/static/media/Union .cecfba40.svg"
+  },
+  {
+    "revision": "ef43c1810bdd75a0797d319ade32bc2b",
+    "url": "/github-pages-demo/static/media/Union.ef43c181.svg"
+  },
+  {
+    "revision": "f5bacdedf1f629869466bfda9a609f30",
+    "url": "/github-pages-demo/static/media/Wrong.f5bacded.svg"
+  },
+  {
+    "revision": "836b7d8ab97639a968cedd0e3c29b04a",
+    "url": "/github-pages-demo/static/media/cool.836b7d8a.svg"
+  },
+  {
+    "revision": "2fec32d4d09d4ff0ef550105c4bce884",
+    "url": "/github-pages-demo/static/media/corect.2fec32d4.mp3"
+  },
+  {
+    "revision": "723faa53fe9b5b988ada058819ce980d",
+    "url": "/github-pages-demo/static/media/hello.723faa53.mp3"
+  },
+  {
+    "revision": "f436bb455bbb85b37a39a476619f17b2",
+    "url": "/github-pages-demo/static/media/hover.f436bb45.svg"
+  },
+  {
+    "revision": "392aeae294da8e39d62595f06ebc0917",
+    "url": "/github-pages-demo/static/media/start.392aeae2.mp3"
+  },
+  {
+    "revision": "5696f0dad44e371dd099b5d954ecaf5c",
+    "url": "/github-pages-demo/static/media/total.5696f0da.mp3"
+  },
+  {
+    "revision": "7772b19be1ce95f62a632b858ae95a35",
+    "url": "/github-pages-demo/static/media/wrong.7772b19b.mp3"
+  }
+]);
